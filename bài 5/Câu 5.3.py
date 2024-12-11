b3d1534ac2f157680839136725660f55d5aae611
TRANVANCUONG64K2 <cuongblue38@gmail.com>
2024-12-11 16:14:27 +0700

    Add files via upload

---
+++ bài 5/Câu 5.3.py
@@ -0,0 +1,7 @@
+﻿print("TRAN VAN CUONG")
+print("MSSV: 235752021610043") 
+print("#############################")
+######################################​
+import numpy as np  # Nhập thư viện NumPy
+x = np.arange(12, 38)
+print(x)
